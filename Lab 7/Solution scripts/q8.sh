81ba803a8974dacdefc25b64da79942949eede00
priyanshu2103 <priyanshu.singh2k@gmail.com>
2018-11-20 02:46:45 +0530

    kd

--- Lab 7/Solution scripts/q8.sh
+++ Lab 7/Solution scripts/q8.sh
@@ -2,6 +2,6 @@
 #install gnumeric
 ssconvert grades.xlsx grades.csv
 ssconvert marks.xlsx marks.csv
-awk 'BEGIN{FS=","}{print $3}' grades.csv | paste -d, marks.csv -> marksheet.csv
+awk 'BEGIN{FS=","}{print $3}' grades.csv | paste -d, marks.csv > marksheet.csv
 cut -d, -f1-3,7- marksheet.csv > marks.csv
 ssconvert marks.csv final_marksheet.xlsx
